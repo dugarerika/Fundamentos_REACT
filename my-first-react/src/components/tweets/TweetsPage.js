8c02fb3fd64829cc071ed51c63c846ad92758bd2
Erika Tavera <dugarerika@gmail.com>
2020-11-29 21:52:25 +0300

    agrego layout

--- my-first-react/src/components/tweets/TweetsPage.js
+++ my-first-react/src/components/tweets/TweetsPage.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { getLatestTweets } from '../../API/tweets'
-import Tweet from './Tweet'
+import React from 'react';
+import { getLatestTweets } from '../../API/tweets';
+import Layout from '../layout';
+import Tweet from './Tweet';
 
 
 class TweetsPage extends React.Component{
@@ -17,24 +18,28 @@ class TweetsPage extends React.Component{
 
     getTweets = async () => { 
         const tweets = await getLatestTweets();
-        this.setState({ tweets });
+        this.setState({tweets});
     }
 
     componentDidMount() { 
         this.getTweets();
     }
 
+    renderContent = () => {
+        const { tweets } = this.state;
+
+        if (!tweets) {
+            return null;
+        }
+        return tweets.map(tweet => <Tweet key={tweet.id} {...tweet} />);
+    };
+
     render() {
-        const {tweets} = this.state;
         return (
-            tweets && (
-            <ul>
-                    {tweets.map(tweet => (
-                        // destructuring
-                        <Tweet key={tweet.id} {...tweet} />
-                ))}
-            </ul>
-            )
+      <Layout {...this.props} title="What's going on...">
+        <div className="tweetsPage">{this.renderContent()}</div>
+      </Layout>
+
         );
     }
 }
