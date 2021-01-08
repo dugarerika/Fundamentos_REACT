e4b1fbc31bcd50519784f048376d668c532552a6
Erika Tavera <dugarerika@gmail.com>
2021-01-08 17:16:58 +0300

    manejando los inputs en el loginPage

--- my-first-react/src/components/auth/LoginPage.js
+++ my-first-react/src/components/auth/LoginPage.js
@@ -4,14 +4,81 @@ import './LoginPage.css';
 import Button from '../shared/Button';
 
 class LoginPage extends React.Component {
+	state = {
+		form: {
+			email: '',
+			password: ''
+		}
+	};
+
+	// haddleChangeEmail = (event) => {
+	// 	this.setState({ email: event.target.value });
+	// };
+
+	// message = () => {
+	// 	const { form: { email } } = this.state;
+	// 	return this.message;
+	// };
+
+	// haddleChangeEmail = (event) => {
+	// 	console.log(event.target.value);
+	// 	this.setState((state) => ({
+	// 		form: { ...state.form, email: event.target.value }
+	// 	}));
+	// };
+
+	// haddleChangePassword = (event) => {
+	// 	console.log(event.target.value);
+	// 	this.setState((state) => ({
+	// 		form: { ...state.form, password: event.target.value }
+	// 	}));
+	// };
+
+	// haddleChangePassword = (event) => {
+	// 	console.log(event.target.value);
+	// 	this.setState((state) => ({
+	// 		form: { ...state.form, password: event.target.value }
+	// 	}));
+	// };
+
+	haddleChange = (event) => {
+		console.log(event.target.value);
+		this.setState((state) => ({
+			form: { ...state.form, [event.target.name]: event.target.value }
+		}));
+	};
+
+	cansubmit = () => {
+		const { form: { email, password } } = this.state;
+		return email && password;
+	};
+
 	render() {
+		const { form: { email, password } } = this.state;
+
+		// const { email, password } = this.state;
+
 		return (
 			<div className='LoginPage'>
 				<h1 className='loginPage-title'>Log in to Twitter</h1>
 				<form>
-					<input type='text' />
-					<input type='password' />
-					<Button type='submit' className='loginPage-button' variant='primary'>
+					<input
+						name='email'
+						type='text'
+						value={email.toUpperCase()}
+						onChange={this.haddleChange}
+					/>
+					<input
+						name='password'
+						type='password'
+						value={password}
+						onChange={this.haddleChange}
+					/>
+					<Button
+						type='submit'
+						className='loginPage-button'
+						variant='primary'
+						disabled={!this.cansubmit()}>
 						Log In
 					</Button>
 				</form>
